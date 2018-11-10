274f33db0d2986bfc3c2adf217706940c38d164e
Toheeb Biala <tab7v@mtmail.mtsu.edu>
2018-11-10 14:36:33 -0600

    Update nonlinear_function.c

--- OpenMP/nonlinear_function.c
+++ OpenMP/nonlinear_function.c
@@ -5,7 +5,7 @@ double *nonlinear_function(double * vector, int num1, int num_thrds){
   int i;
   return_func = allocate_vector(num1);
   zero_vector(return_func, num1, num_thrds);
-#pragma omp paralle for private(i) num_threads(num_thrds)
+#pragma omp parallel for private(i) num_threads(num_thrds)
   for (i=0; i <num1; i++){
     return_func[i] = pow(vector[i],2);
   }
